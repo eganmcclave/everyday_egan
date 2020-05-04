29bbb69c9d47c9ca1b03cb04de6dbf9d939e5f47
Egan McClave <eganmcclave@yahoo.com>
2020-05-04 15:27:28 -0400

    Created 'image_alignment.py' to focus on cropping images based on info from the facial detection and then compiling the images into a .mp4 file.

---
+++ code/image_alignment.py
@@ -0,0 +1,65 @@
+
+# Standard library imports
+import glob
+import os
+
+# Third party library imports
+import ffmpeg
+import numpy
+import glob
+import PIL
+
+# Local library imports
+from HOG_implementation.facial_detection import *
+
+
+def write_to_video(manip_photo_dir):
+  (
+    ffmpeg
+    .input(os.path.join(manip_photo_dir, '*.jpeg'), pattern_type='glob', framerate=5)
+    .output('movie.mp4')
+    .run()
+  )
+
+
+def jpeg_crop_images(jpeg_faces_path, faces_dict):
+  pbar = tqdm(glob.glob(os.path.join(jpeg_faces_path, '*.jpeg')))
+  for file_path in pbar:
+    file_name = os.path.split(file_path)[1]
+    pbar.set_description('Cropping {}'.format(file_name))
+
+    img = PIL.Image.open(file_path)
+    face_dict = faces_dict[file_name] 
+
+    img = crop_image(img, face_dict)
+    img.save(file_path)
+
+
+def crop_image(image, face_dict, box_size=2000):
+  point = face_dict[0]['facial_points'][27]
+  coords = (
+    point[0] - 0.50 * box_size, point[1] - 0.35 * box_size,
+    point[0] + 0.50 * box_size, point[1] + 0.65 * box_size
+  )
+
+  image = image.crop(coords)
+  return image
+
+
+if __name__ == '__main__':
+  
+  # Standard library imports
+  from configparser import ConfigParser
+
+  # set up configuration and initialize variables
+  config = ConfigParser()
+  config.read('../config.ini')
+
+  predictor_path = config['Paths']['HOG_predictor_path']
+  manip_photo_dir = config['Paths']['manipulated_dir']
+  
+  faces_dict = jpeg_facial_detection(predictor_path, manip_photo_dir, draw_bool=False)
+  jpeg_crop_images(manip_photo_dir, faces_dict)
+  write_to_video(manip_photo_dir)
+
+
