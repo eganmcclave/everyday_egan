93c262a8baf482ca3e356e5b780211b9ae6904dc
Egan McClave <eganmcclave@yahoo.com>
2020-06-13 23:09:33 -0400

    Update video writer code to process numpy array
    This commit adds the functionality to process numpy arrays into a video.
    This un-edited function comes from the ffmpeg-python github repo.

--- code/image_alignment.py
+++ code/image_alignment.py
@@ -38,6 +38,27 @@ def write_to_video(jpeg_photo_dir, video_name='video', frame_rate=5):
     print(err)
 
 
+def vidwrite(fn, images, framerate=60, vcodec='libx264'):
+    if not isinstance(images, np.ndarray):
+        images = np.asarray(images)
+    n,height,width,channels = images.shape
+    process = (
+        ffmpeg
+            .input('pipe:', format='rawvideo', pix_fmt='rgb24', s='{}x{}'.format(width, height))
+            .output(fn, pix_fmt='yuv420p', vcodec=vcodec, r=framerate)
+            .overwrite_output()
+            .run_async(pipe_stdin=True)
+    )
+    for frame in images:
+        process.stdin.write(
+            frame
+                .astype(np.uint8)
+                .tobytes()
+        )
+    process.stdin.close()
+    process.wait()
+
+
 def jpeg_crop_images(jpeg_faces_path, faces_dict):
   """ Coordinates the cropping of images in a directory
 
